57e6dea1e862ee3284beb6f16af4912eb8dedca1
Ash Joseph <kamikazedesigns@gmail.com>
2024-11-02 12:22:18 +0000

    lazy nvim was broken apparently IDK

--- dot_config/nvim/lua/config/keymaps.lua
+++ dot_config/nvim/lua/config/keymaps.lua
@@ -1,5 +1,3 @@
 -- Keymaps are automatically loaded on the VeryLazy event
--- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/confvim.keymap.setig/keymaps.lua
+-- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
-vim.keymap.set("n", ";", "<cmd>:<cr>")
-vim.keymap.set("n", ";", ":", { desc = "CMD enter command mode" })
